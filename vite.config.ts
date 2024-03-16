7b0178b044d8d49a6e784c327548ad289ad283f6
hyrious <hyrious@outlook.com>
2024-03-16 14:45:57 +0800

    fix: base path in website

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,16 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
+  base: '',
+  build: {
+    sourcemap: true,
+    modulePreload: {
+      polyfill: false
+    },
+  },
   define: {
     '__DEV__': 'true',
     '__COMMIT__': '"HEAD"',
     '__VERSION__': '"dev"',
-  }
+  },
 })
